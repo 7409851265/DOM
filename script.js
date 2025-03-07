7a49b69ab9d4d1228cd6b284c8a52cf4638ccad7
zafar mirza <mirzazafar543@gmail.com>
2025-03-08 03:10:11 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,30 @@
+let buttons = document.getElementsByTagName('button');
+let inputDisplay = document.getElementById('display');
+
+let equal = document.getElementById('equal');
+
+let clear = document.getElementById('clear');
+
+let input = '';
+
+btnValues = Array.from(buttons);
+
+btnValues.forEach(button => {
+    button.addEventListener('click', function (e){
+        input = e.target.innerText
+
+        if (input !== "=" && input !== "C") {
+            inputDisplay.value = inputDisplay.value + input;
+
+            input = inputDisplay.value;
+        }
+
+    })
+    equal.addEventListener('click', function(){
+        inputDisplay.value = eval(input);
+    })
+
+    clear.addEventListener('click', function(){
+        inputDisplay.value = "";
+    })
+});
